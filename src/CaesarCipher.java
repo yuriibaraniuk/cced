bda8d7b0ee46bd9fe1d0246ed9a48ac50f8162aa
Yurii Baraniuk <yuriibaraniuk@yahoo.com>
2023-03-12 12:12:29 +0200

    Implemented CaesarCipher class

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -1,2 +1,28 @@
 public class CaesarCipher {
+
+    private static final String ALPHABET =   "abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+            "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" + "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ" +
+            "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя" + "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЮЯ" +
+            ".,\":!? +-*/\\@#$%^&(){}[];'|`~=_©«»—" + "0123456789";
+
+    public String encrypt(String message, int key) {
+        StringBuilder builder = new StringBuilder();
+        for (char aChar : message.toCharArray()) {
+            int index = ALPHABET.indexOf(aChar);
+            if (index >= 0) {
+                int newIndex = (index + key) % ALPHABET.length();
+                char charAt = newIndex < 0 ? ALPHABET.charAt(ALPHABET.length() + newIndex) : ALPHABET.charAt(newIndex);
+                builder.append(charAt);
+            }
+        }
+        return builder.toString();
+    }
+
+    public String decrypt(String message, int key) {
+        return encrypt(message, key * (-1));
+    }
+
+    public int alphabetLength() {
+        return ALPHABET.length();
+    }
 }

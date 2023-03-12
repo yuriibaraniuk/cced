4311259fc7068de38440425463a814dfa5efe371
Yurii Baraniuk <yuriibaraniuk@yahoo.com>
2023-03-12 12:16:15 +0200

    Implemented Decrypted class

--- src/Decrypted.java
+++ src/Decrypted.java
@@ -1,2 +1,32 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Decrypted {
+    private final Scanner scanner = new Scanner(System.in);
+    private final CaesarCipher caesarCipher = new CaesarCipher();
+
+    public void decrypted() throws IOException {
+
+        System.out.println("Введіть повний шлях до файлу для його розшифровки:");
+        String pathEncryptedFile = scanner.nextLine();
+
+        System.out.println("Введіть ключ шифрування:");
+        int key = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Введіть повний шлях до файлу, до якого записати розшифрований текст:");
+        String pathNotEncryptedFile = scanner.nextLine();
+
+        try (var reader = Files.newBufferedReader(Paths.get(pathEncryptedFile));
+             var writer = Files.newBufferedWriter(Paths.get(pathNotEncryptedFile))
+        ) {
+            while (reader.ready()) {
+                String string = reader.readLine();
+                String decryptString = caesarCipher.decrypt(string, key);
+                writer.write(decryptString + System.lineSeparator());
+            }
+        }
+        System.out.println("Вміст файлу розшифровано." + System.lineSeparator());
+    }
 }

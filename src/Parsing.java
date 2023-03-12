89311542276ce20e53303825c6c98676a028d38a
Yurii Baraniuk <yuriibaraniuk@yahoo.com>
2023-03-12 12:25:21 +0200

    Implemented Parsing class

--- src/Parsing.java
+++ src/Parsing.java
@@ -1,2 +1,76 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
 public class Parsing {
+    private final Scanner scanner = new Scanner(System.in);
+
+    private final Map<Character, Integer> encrypted = new HashMap<>();
+    private final Map<Character, Integer> statistic = new HashMap<>();
+    private final Map<Character, Character> decrypted = new HashMap<>();
+
+    public void parse() throws IOException {
+
+        System.out.println("Введіть повний шлях до файлу для його розшифровки:");
+        String pathEncrypted = scanner.nextLine();
+
+        System.out.println("Введіть повний шлях до файлу для набору статистики:");
+        String pathStatistic = scanner.nextLine();
+
+        Path parsing = PathHelper.buildFileName(pathEncrypted, "_parsing");
+
+        List<Map.Entry<Character, Integer>> listEncrypted = mapToList(fillMapValues(encrypted, pathEncrypted));
+        List<Map.Entry<Character, Integer>> listStatistic = mapToList(fillMapValues(statistic, pathStatistic));
+
+        if (listEncrypted.size() <= listStatistic.size()) {
+            for (int i = 0; i < listEncrypted.size(); i++) {
+                decrypted.put(listEncrypted.get(i).getKey(), listStatistic.get(i).getKey());
+            }
+        } else {
+            System.out.println("Розмір файлу статистики недостатній для розшифровки, необхідний файл більшої довжини, ніж зашифрований" + System.lineSeparator());
+        }
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(pathEncrypted));
+             BufferedWriter writer = Files.newBufferedWriter(parsing)) { // decryption
+            while (reader.ready()) {
+                StringBuilder builder = new StringBuilder();
+                String string = reader.readLine();
+                for (char encryptedChar : string.toCharArray()) {
+                    Character decryptedChar = decrypted.get(encryptedChar);
+                    builder.append(decryptedChar);
+                }
+                writer.write(builder + System.lineSeparator());
+            }
+        }
+        System.out.println("Вміст файлу розшифрований методом статистичного аналізу." + System.lineSeparator());
+    }
+
+    private Map<Character, Integer> fillMapValues(Map<Character, Integer> map, String path) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(path))) {
+            while (reader.ready()) {
+                String string = reader.readLine();
+                builder.append(string);
+            }
+            for (char aChar : builder.toString().toCharArray()) {
+                if (!map.containsKey(aChar)) {
+                    map.put(aChar, 1);
+                } else {
+                    map.put(aChar, map.get(aChar) + 1);
+                }
+            }
+            return map;
+        }
+    }
+
+    private List<Map.Entry<Character, Integer>> mapToList(Map<Character, Integer> map) {
+        List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
+
+        Comparator<Map.Entry<Character, Integer>> comparator = Map.Entry.comparingByValue();
+
+        list.sort(comparator.reversed());
+
+        return list;
+    }
 }
